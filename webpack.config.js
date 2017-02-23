03357af6dddc1b36819d3bd24d9e3cbdb1b57479
Maxislav <lmaxim@mai.ru>
2017-02-23 22:49:46 +0200

    olol

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 "use strict";
-const NODE_PATH="/usr/local/lib/node_modules"
+//const NODE_PATH="/usr/local/lib/node_modules"
 const NODE_ENV = process.env.NODE_ENV || 'prod';
 const webpack = require('webpack');
 const path = require('path')
@@ -7,19 +7,20 @@ const path = require('path')
 console.log(NODE_ENV)
 module.exports = {
   entry: {
-    init:['webpack-dev-server/client',__dirname+"/src/init.js"]
+	 app: ["./src/init.js"]
+    //init:['webpack-dev-server/client',__dirname+"/src/init.js"]
   },
   output: {
-    path:  "/dist/",
-    filename: "borispol-radar.min.js",
-    library: "vue"
+   path: path.resolve(__dirname, "build"),
+    publicPath: "/assets/",
+    filename: "bundle.js"
   },
  
   watch: NODE_ENV == 'dev',
   watchOptions: {
     aggregateTimeout: 100
   },
-  devtool: NODE_ENV == 'dev' ? 'source-map' : false,
+  devtool: 'source-map',// NODE_ENV == 'dev' ? 'source-map' : false,
   plugins: [
       new webpack.DefinePlugin({
         NODE_ENV: JSON.stringify(NODE_ENV)
@@ -30,10 +31,11 @@ module.exports = {
       {
         test: /\.js$/,
         //include: __dirname ,
-        loader: 'babel-loader',
-        query: {
+        loader: 'babel-loader'
+        /*query: {
           presets: ['es2015']
         }
+*/
       },
       { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' }
     ]
@@ -47,17 +49,18 @@ module.exports = {
   }
   ,
   devServer: {
-    contentBase: [path.join(__dirname), path.join(__dirname, 'node_modules')],
+    //contentBase: [path.join(__dirname), path.join(__dirname, 'node_modules')],
 
    // contentBase: ,
-    inline: true 
+   // inline: true ,
+//	hot: true
   }
 
 
 
 };
 
-if(NODE_ENV!='dev'){
+if(NODE_ENV=='dev'){
   module.exports.plugins.push(
     new webpack.optimize.UglifyJsPlugin({
       compress: {
