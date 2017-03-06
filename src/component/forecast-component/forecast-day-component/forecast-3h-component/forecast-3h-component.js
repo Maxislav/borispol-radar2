3e63b93d89288d596307dc90481498a47dd9fd89
Maxim Lipatov <lmaxim@mail.ru>
2017-03-06 19:39:31 +0200

    forecast build component

--- src/component/forecast-component/forecast-day-component/forecast-3h-component/forecast-3h-component.js
+++ src/component/forecast-component/forecast-day-component/forecast-3h-component/forecast-3h-component.js
@@ -1,14 +1,33 @@
 import Vue from 'vue';
 import './forecast-3h-component.styl';
-import template from  './forecast-3h-component.jade'
+import template from  './forecast-3h-component.jade';
+import dateFormat from 'dateformat';
 
 export default  Vue.component('forecast-3h-component', {
 	props: ['hh'],
 	template: template(),
 	data: function () {
-		//console.log(this.hh)
+		var R = {
+			color: {
+				hh00: '#99BCFF',
+				hh03: '#CCDDFF',
+				hh06: '#CDF',
+				hh09: '#E6EEFF',
+				hh12: '#E6EEFF',
+				hh15: '#E6EBF5',
+				hh18: '#CCDDFF',
+				hh21: '#B3CDFF'
+			}
+		};
+
+		const date = new Date(this.hh['dt_txt']);
+		const temp = Math.round(this.hh.main.temp)
+
+		const HH ='hh'+ dateFormat(date, 'HH');
 		return{
-			temp: this.hh.main.temp
+			date,
+			temp: (temp<0 ? '-'+temp : '+'+temp)  +'&deg;C',
+			color: R.color[HH]
 		}
 	}
 	
