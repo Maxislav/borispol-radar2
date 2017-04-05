1a8243cb4aea45affa5a224747e87874c11fa64f
Maxim Lipatov <lmaxim@mail.ru>
2017-04-05 12:41:06 +0300

    clear bad pixel

--- src/component/ukbb-calc-component/ukbb-calc-component.js
+++ src/component/ukbb-calc-component/ukbb-calc-component.js
@@ -10,85 +10,78 @@ import {getDirection} from './get-direction';
 import {PixelData, pixelArray, toRain} from './pixel-data';
 
 
-
-
-
-
-
-
 const toLngLat = {
-	lng: (x)=>{
-	 return	x*(33.89 - 27.9)/515 + 27.9
+	lng: (x) => {
+		return x * (33.89 - 27.9) / 515 + 27.9
 	},
-	lat: (y)=>{
-		return (470 - y)*(52.14-48.35)/470 + 48.35
+	lat: (y) => {
+		return (470 - y) * (52.14 - 48.35) / 470 + 48.35
 	}
 };
 
 
-const calc = (rain, original, a)=>{
+const calc = (rain, original, a) => {
 
 
-	if(a){
+	if (a) {
 		a = Math.normalizeDegree(a);
 		const {x, y} = original;
-        rain = rain.filter(p=>{
-        	let _a;
-        	if(x<p.x && p.y<y){
-				_a = (Math.atan((p.x - x)/(y-p.y)))
-			}else if(x<p.x && y<p.y ){
-        		_a = (Math.atan( (p.y - y)/(p.x - x) ))+Math.PI/2
-			}else if(p.x<x && y<p.y ){
-                _a = (Math.atan( (x-p.x)/(p.y-y) ))+Math.PI
-			}else {
-                _a = (Math.atan( (y - p.y )/(x-p.x) ))+3*Math.PI/2
+		rain = rain.filter(p => {
+			let _a;
+			if (x < p.x && p.y < y) {
+				_a = (Math.atan((p.x - x) / (y - p.y)))
+			} else if (x < p.x && y < p.y) {
+				_a = (Math.atan((p.y - y) / (p.x - x))) + Math.PI / 2
+			} else if (p.x < x && y < p.y) {
+				_a = (Math.atan((x - p.x) / (p.y - y))) + Math.PI
+			} else {
+				_a = (Math.atan((y - p.y ) / (x - p.x))) + 3 * Math.PI / 2
 			}
 			_a = Math.degrees(_a);
-			return Math.abs(a-_a)<15
+			return Math.abs(a - _a) < 15
 		})
 	}
 
-    const filterByColor = rain.filter((item)=>{
-        return constantRadarColor.find((val)=>{
-        	const find =  Math.abs(item.colorDec - val.colorDec) < 1000
-        	if(find){
-        		item.text = val.text
+	const filterByColor = rain.filter((item) => {
+		return constantRadarColor.find((val) => {
+			const find = Math.abs(item.colorDec - val.colorDec) < 1000
+			if (find) {
+				item.text = val.text
 			}
-            return find
-        });
-    });
+			return find
+		});
+	});
 
-    filterByColor.forEach(r => {
-        r.distFrom(original.x, original.y)
-    });
+	filterByColor.forEach(r => {
+		r.distFrom(original.x, original.y)
+	});
 
-    filterByColor.sort((a, b) => {
-        if (a.dist < b.dist) {
-            return -1
-        }
-        if (b.dist < a.dist) {
-            return 1
-        }
-        if (a.dist == b.dist) {
-            return 0
-        }
-    });
+	filterByColor.sort((a, b) => {
+		if (a.dist < b.dist) {
+			return -1
+		}
+		if (b.dist < a.dist) {
+			return 1
+		}
+		if (a.dist == b.dist) {
+			return 0
+		}
+	});
 
-    const colors = [];
+	const colors = [];
 
 
+	return filterByColor.filter(function (value, index, arr) {
+		const find = colors.find((val) => {
+			return Math.abs(value.colorDec - val) < 10
+		});
+		if (!find && (100 < value.r || 100 < value.g || 100 < value.b )) {
+			colors.push(value.colorDec);
 
-    return filterByColor.filter(function (value, index, arr) {
-        const find = colors.find((val) => {
-            return Math.abs(value.colorDec - val) < 10
-        });
-        if (!find && (100<value.r || 100<value.g || 100<value.b )) {
-            colors.push(value.colorDec);
-
-            return true
-        }
-        return false
-    });
+			return true
+		}
+		return false
+	});
 
 };
 
@@ -98,69 +91,71 @@ export default {
 	data: function () {
 		this.drag = false;
 		this.layer = {
-			x:0,
-			y:0
+			x: 0,
+			y: 0
 		};
 
 		this.windDirection = null;
 
 		this.original = {
 			x: 0,
-			y:0
+			y: 0
 		};
 
 		this.image = null;
 		const iam = {
 			x: this.$storage.getItem('flag-x') || 680,
-			y: this.$storage.getItem('flag-y') ||  0
+			y: this.$storage.getItem('flag-y') || 0
 		};
 
 
 		this._rain = [];
 
-		this.toOriginal = ()=>{
+		this.toOriginal = () => {
 			return {
-				x:  (this.iam.x+5)*(this.image.naturalWidth/this.image.width),
-				y: (this.iam.y+22)*(this.image.naturalHeight/this.image.height)
+				x: (this.iam.x + 5) * (this.image.naturalWidth / this.image.width),
+				y: (this.iam.y + 22) * (this.image.naturalHeight / this.image.height)
 			}
 		};
 
 
 		const lngLat = {
-			lng:toLngLat.lng(iam.x+5),
-			lat:toLngLat.lat(iam.y+22)
+			lng: toLngLat.lng(iam.x + 5),
+			lat: toLngLat.lat(iam.y + 22)
 		};
 
-		const onload = (image) =>{
-				this.image = image;
-				const canvas = document.createElement("canvas");
-				canvas.width = image.naturalWidth;
-				canvas.height = image.naturalHeight;
-				if (this.canvasDirection) this.canvasDirection.destroy()
-				this.canvasDirection = new CanvasDirection(canvas);
-				image.parentNode.appendChild(this.canvasDirection.el);
+		const onload = (image) => {
+			this.image = image;
+			const canvas = document.createElement("canvas");
+			canvas.width = image.naturalWidth;
+			canvas.height = image.naturalHeight;
+			if (this.canvasDirection) this.canvasDirection.destroy()
+			this.canvasDirection = new CanvasDirection(canvas);
+			image.parentNode.appendChild(this.canvasDirection.el);
 
-				const context = canvas.getContext("2d");
-				context.drawImage(image, 0, 0);
-				const imageData = context.getImageData(0, 0, 500, canvas.height );
-				context.clearRect(0,0, canvas.width , canvas.height);
-				context.putImageData(imageData,0,0);
-				const data = pixelArray(imageData);
-				this.windDirection = getDirection(data);
-				console.log('windDirection',this.windDirection)
-				this._rain.length = 0;
-				toRain(data, this._rain);
-				if(this.iam.x<500){
-					const origin = this.toOriginal();
-					this.original.x = origin.x;
-					this.original.y = origin.y;
-					if(this.windDirection!=null){
-							this.canvasDirection.draw(this.original.x, this.original.y, this.windDirection+180);
-					}
-					this.rain.length = 0;
-					calc(this._rain, this.original, this.windDirection !==null ? this.windDirection+180 : null).forEach(r=>this.rain.push(r))
+			const context = canvas.getContext("2d");
+			context.drawImage(image, 0, 0);
+			context.clearRect(20,470, 10,10);
+			const imageData = context.getImageData(0, 0, 500, canvas.height);
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			context.putImageData(imageData, 0, 0);
+
+			const data = pixelArray(imageData);
+			this.windDirection = getDirection(data);
+			console.log('windDirection', this.windDirection)
+			this._rain.length = 0;
+			toRain(data, this._rain);
+			if (this.iam.x < 500) {
+				const origin = this.toOriginal();
+				this.original.x = origin.x;
+				this.original.y = origin.y;
+				if (this.windDirection != null) {
+					this.canvasDirection.draw(this.original.x, this.original.y, this.windDirection + 180);
 				}
-			};
+				this.rain.length = 0;
+				calc(this._rain, this.original, this.windDirection !== null ? this.windDirection + 180 : null).forEach(r => this.rain.push(r))
+			}
+		};
 
 		return {
 			onload,
@@ -173,12 +168,12 @@ export default {
 		mousemove: function (e) {
 
 
-			if(this.original.x<500){
+			if (this.original.x < 500) {
 
 			}
 
-			if(!this.drag) return;
-			this.iam.x = e.clientX- this.container.x - this.layer.x;
+			if (!this.drag) return;
+			this.iam.x = e.clientX - this.container.x - this.layer.x;
 			this.iam.y = e.clientY - this.container.y - this.layer.y;
 			//this.lngLat.lng = this.iam.x*(33.89 - 27.9)/515 + 27.9;
 			//this.lngLat.lat = toLngLat.lat(this.iam.y);
@@ -190,19 +185,19 @@ export default {
 			this.lngLat.lat = toLngLat.lat(this.original.y)
 			this.lngLat.lng = toLngLat.lng(this.original.x)
 
-			if(this.windDirection!=null)
-				this.canvasDirection.draw(this.original.x, this.original.y, this.windDirection+180);
+			if (this.windDirection != null)
+				this.canvasDirection.draw(this.original.x, this.original.y, this.windDirection + 180);
 
 			this.rain.length = 0;
-			if(this.iam.x<500)
-				calc(this._rain, this.original,this.windDirection !==null ? this.windDirection+180 : null).forEach(r=>this.rain.push(r))
+			if (this.iam.x < 500)
+				calc(this._rain, this.original, this.windDirection !== null ? this.windDirection + 180 : null).forEach(r => this.rain.push(r))
 
 		},
 		mousedown: function (e) {
 
 
-			this.layer.x = e.layerX-(document.body.scrollLeft||0);
-			this.layer.y = e.layerY-(document.body.scrollTop||0);
+			this.layer.x = e.layerX - (document.body.scrollLeft || 0);
+			this.layer.y = e.layerY - (document.body.scrollTop || 0);
 
 			this.drag = true;
 		}
@@ -214,11 +209,11 @@ export default {
 		this.container = $(this.$el).find('.drawable-container');
 		this.container.x = position(this.container[0]).x;
 		this.container.y = position(this.container[0]).y;
-		this._mouseup = (e)=>{
+		this._mouseup = (e) => {
 			this.drag = false;
 
-			this.$storage.setItem('flag-x', this.iam.x );
-			this.$storage.setItem('flag-y', this.iam.y )
+			this.$storage.setItem('flag-x', this.iam.x);
+			this.$storage.setItem('flag-y', this.iam.y)
 		}
 		document.addEventListener('mouseup', this._mouseup)
 
