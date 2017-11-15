4dfe6ec246810767b27eb2f8eb5d09f73712300e
Maxim Lipatov <lmaxim@mail.ru>
2017-11-15 12:04:16 +0200

    ws try

--- src/component/setting-component/setting-component.js
+++ src/component/setting-component/setting-component.js
@@ -2,7 +2,37 @@ import Vue from 'vue';
 import template from './setting-component.pug';
 import './setting-component.styl';
 
-let scope = null
+let scope = null;
+
+let port = null
+
+const onMessage = (e)=>{
+	const mess = e.data;
+	console.log(mess)
+	port = event.ports[0]
+	//event.ports[0].postMessage("SW Says 'Hello back!'")
+};
+
+const postMessage = (text) =>{
+	if(!port){
+		port = navigator.serviceWorker.controller
+		if(port){
+			port.postMessage("SW Says 'Hello back!'")
+		}else {
+			navigator.serviceWorker.ready
+				.then(d=>{
+					port.postMessage("SW Says 'Hello back!'")
+				})
+				.catch(err=>{
+					console.log(err)
+				})
+		}
+
+	}else {
+		port.postMessage("SW Says 'Hello back!'")
+	}
+
+}
 
 window.addEventListener('load', ()=>{
 	if ('serviceWorker' in navigator) {
@@ -10,17 +40,16 @@ window.addEventListener('load', ()=>{
 			for(let registration of registrations) {
 				registration.unregister()
 			}
+
+			/**
+			 * {ServiceWorkerContainer}
+			 */
+			navigator.serviceWorker;
 			navigator.serviceWorker.register('/sw.js').then(function(registration) {
 				// Registration was successful
-				scope = registration.scope
-				console.log('ServiceWorker registration successful with scope: ', registration.scope);
-
-
-				navigator.serviceWorker.addEventListener('message', function(event){
-					console.log(event.data);
-					//event.ports[0].postMessage("Client 1 Says 'Hello back!'");
-				});
-
+				scope = registration.scope;
+				console.log('ServiceWorker registration successful');
+				navigator.serviceWorker.addEventListener('message', onMessage);
 
 			}, function(err) {
 				// registration failed :(
@@ -65,8 +94,8 @@ export const SettingComponent = Vue.component('setting-component', {
 			},
 
 			onNotify: function () {
-
-				navigator.serviceWorker.controller.postMessage("Client 1 says ");
+				postMessage("Client 1 says")
+				//navigator.serviceWorker.controller.postMessage("Client 1 says ");
 
 
 
@@ -80,19 +109,6 @@ export const SettingComponent = Vue.component('setting-component', {
 						} });*/
 
 
-				setTimeout(function () {
-
-				/*	if ('serviceWorker' in navigator) {
-						navigator.serviceWorker.register('/sw.js').then(function(registration) {
-							// Registration was successful
-							console.log('ServiceWorker registration successful with scope: ', registration.scope);
-						}, function(err) {
-							// registration failed :(
-							console.log('ServiceWorker registration failed: ', err);
-						});
-					};*/
-
-				}, 1000)
 
 			}
 			/*	Notification.requestPermission().then(function(result) {
--- sw.js
+++ sw.js
@@ -5,6 +5,83 @@ const CURRENT_CACHES = {
 };
 let socket;
 
+
+class $Promise extends Promise {
+	constructor(f) {
+		let res = null;
+		let rej = null;
+		let r, j;
+		let _f = f;
+		if(!_f){
+			_f = function (_res, _rej) {
+				res = _res;
+				rej = _rej;
+				if(r!==undefined) res(r);
+				if(j!==undefined) rej(j);
+			}
+		}
+		super(_f);
+		this.resolve = (_r)=>{
+			r = _r;
+			if(res) res(_r);
+			return this;
+		};
+
+		this.reject = (_j)=>{
+			j = _j;
+			if(rej) rej(_j);
+			return this
+		};
+	}
+}
+
+class Socket extends WebSocket{
+	constructor(...args){
+		super(...args);
+		this._events = {};
+		this._ready = false;
+		this._r = null;
+		this.onopen = (e)=>{
+			console.log("Соединение установлено.");
+
+			this._ready = true;
+			if(this._r) this._r(this);
+		};
+		this.onmessage = (e)=>{
+			this._events['message'] = this._events['message'] || []
+			this._events['message'].forEach(f=>{
+				f(e)
+			});
+			return this;
+		};
+	}
+	on(eName, f){
+		this._events[eName] = this._events[eName] || []
+		this._events[eName].push(f);
+		return this;
+	}
+	off(eName, f){
+		this._events[eName] = this._events[eName] || []
+		if(f){
+			const index = this._events[eName].findIndex(f)
+			if(-1<index){
+				this._events[eName].splice(index,1)
+			}
+		}else {
+			while (this._events[eName].length){
+				this._events[eName].pop()
+			}
+		}
+		return this
+	}
+	then(r){
+		this._r = r;
+		if(this._ready) this._r(this);
+		return this
+	}
+}
+
+
 let i = 0;
 
 self.addEventListener('install', function(event) {
@@ -21,9 +98,24 @@ self.addEventListener('install', function(event) {
 
 
 
-	socket = new WebSocket("ws://localhost:8082")
+	socket =  new Socket("ws://localhost:8082")
+	socket.on('message', (e)=>{
+		console.log("сокет сообщение получено ", e.data)
+	});
 
-	setInterval(()=>{
+
+
+
+
+	/*socket.onopen = function() {
+		console.log("Соединение установлено.")
+		socketPromise.resolve(socket)
+
+	};
+*/
+
+
+	/*setInterval(()=>{
 		self.clients.matchAll({includeUncontrolled: true, type: 'window'})
 			.then(clients=>{
 				//console.log(clients)
@@ -31,8 +123,8 @@ self.addEventListener('install', function(event) {
 					client.postMessage('Client postMessage ' + i++)
 				})
 			})
-	}, 2000)
-	self.skipWaiting()
+	}, 2000)*/
+	//self.skipWaiting()
 });
 
 self.addEventListener('activate', function(event) {
@@ -43,8 +135,9 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('message', function(event){
 	console.log("SW Received Message: " + event.data);
-	//console.log(WebSocket)
-	socket.send("Привет");
+
+
+
 
 
 
