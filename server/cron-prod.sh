95e225fcf94c88c11d9d2dab7bf8cd11bbfcf010
Maxim Lipatov <lmaxim@mail.ru>
2018-03-23 11:47:26 +0200

    static ts

--- server/cron-prod.sh
+++ server/cron-prod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /var/www/
-./node_modules/.bin/ts-node -- ./borispol-radar-server/save-image.cron.ts --rootdir ../
+./node_modules/.bin/ts-node -- ./borispol-radar-server/save-image.cron.ts --rootdir ../dist
