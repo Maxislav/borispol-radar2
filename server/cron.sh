4a1760770c46d544f43c4120f9d2749aed575a1f
Maxim Lipatov <lmaxim@mail.ru>
2018-04-05 17:58:59 +0300

    fb tags

--- server/cron.sh
+++ server/cron.sh
@@ -1,3 +1,2 @@
-#!/usr/bin/env bash
-cd /home/max/www/borispol-radar2
-./node_modules/.bin/ts-node -- ./server/save-image.cron.ts --rootdir ../src
+cd /home/maxislav/www/borispol-radar2
+/home/maxislav/.nvm/versions/node/v8.9.1/bin/node ./node_modules/.bin/ts-node -- ./server/save-image.cron.ts --rootdir ../src
