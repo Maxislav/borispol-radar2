c34f938391701973f20404f169ea3595b9787221
Maxim Lipatov <lmaxim@mail.ru>
2018-04-27 12:41:25 +0300

    cron

--- server/cron.sh
+++ server/cron.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 cd /home/maxislav/www/borispol-radar2
-/home/maxislav/.nvm/versions/node/v8.9.1/bin/node ./node_modules/.bin/ts-node -- ./server/save-image.cron.ts --rootdir ../src
+/usr/local/bin/node ./node_modules/.bin/ts-node -- ./server/save-image.cron.ts --rootdir ../src
