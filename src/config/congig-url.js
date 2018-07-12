811b7f7c8570fda1bc51d09ea7b190dd16248414
maxim <maximmalyshyn@gmail.com>
2018-07-12 11:56:35 +0300

    "types": [           "node"         ]

--- src/config/congig-url.js
+++ src/config/congig-url.js
@@ -4,20 +4,20 @@ let url = {
 		ir: 'img/ir/',
 		vi: 'img/vi/',
 		//ukbb: '/ppp/borisbolukbb',
-		ukbb: 'http://localhost:8085/borisbolukbb',
+		ukbb: 'http://localhost:8084/borisbolukbb',
 		//ukbb: 'img/meteoradar_borispol.png',
 		//"ukbb-history": "/proxy-history/loadUkbbHistory.php",
 		//"ukbb-history": "http://localhost:8085/loadUkbbHistory",
-		"ukbb-history": "http://178.62.44.54:8085/loadUkbbHistory",
+		"ukbb-history": "http://178.62.44.54:8084/loadUkbbHistory",
 		"upload": '/upload.php'
 	},
 	production: {
 		ir: 'img/ir/',
 		vi: 'img/vi/',
 		//ukbb: 'php/ukbb-latest.php',
-    ukbb: 'http://178.62.44.54:8085/borisbolukbb',
+    ukbb: 'http://178.62.44.54:8084/borisbolukbb',
 		//"ukbb-history": "php/loadUkbbHistory.php",
-    "ukbb-history": "http://178.62.44.54:8085/loadUkbbHistory",
+    "ukbb-history": "http://178.62.44.54:8084/loadUkbbHistory",
 		"upload": 'php/upload.php'
 	}
 };
