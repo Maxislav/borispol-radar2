4b60abf3d28567948702d054c17c1a7725619705
maxim <maximmalyshyn@gmail.com>
2018-07-12 12:13:19 +0300

    "types": [           "node"         ]

--- src/constant/constant-url.js
+++ src/constant/constant-url.js
@@ -1,5 +1,5 @@
 const url = {
-    prod:  'http://178.62.44.54:8085',
+    prod:  'http://178.62.44.54:8084',
     dev: `${window.location.protocol}//${window.location.hostname}:${8084}`
 };
 export const socketUrl = NODE_ENV == 'dev' ? url.dev : url.prod
