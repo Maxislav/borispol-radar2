27354cd7ee2a4c667cec7d773319d07a75ff8bae
maxim <maximmalyshyn@gmail.com>
2018-07-13 10:15:21 +0300

    cron

--- server/cron-prod.sh
+++ server/cron-prod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /var/www/borispol-radar/
-./node_modules/.bin/ts-node -- ./server/save-image.cron.ts --rootdir ../dist
+./node_modules/.bin/ts-node --project .server/tsconfig.json ./server/save-image.cron.ts --rootdir ../dist
