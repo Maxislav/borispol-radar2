d76d98b848fee36b15fa648e822e4dec70f1bfe1
Maxislav <gliderman2>
2020-08-11 13:15:44 +0300

    debug

--- server/borispolukbb.ts
+++ server/borispolukbb.ts
@@ -12,8 +12,8 @@ function httpGet<T>(opt: {path: string, host: string}, count = 0) {
             headers: {
                 Host: 'meteoinfo.by',
                 Referer: `http://${opt.host}${opt.path}`,
-                Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
+               // Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                //'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
             }
         }, (resp: IncomingMessage) => {
             const chunks: Uint8Array[] = [];
