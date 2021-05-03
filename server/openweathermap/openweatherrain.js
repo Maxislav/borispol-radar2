c759c4afc8412119a3227445810192c36c3bc202
Maxislav <Maksym.Lipatov@McKesson.com>
2021-05-04 01:33:53 +0300

    replace openweathermap.component.html 2error 2

--- server/openweathermap/openweatherrain.js
+++ server/openweathermap/openweatherrain.js
@@ -14,28 +14,43 @@ const getMin = (date, offset) => {
     return `${m}`;
 };
 //https://b.sat.owm.io/maps/2.0/radar/7/75/43?appid=9de243494c0b295cca9337e1e96b00e2&day=2021-05-02T22:00
-const loadRadar = (count) => {
-    console.log(count);
+function jimpCreate256() {
+    return new Promise((resolve) => {
+        new Jimp(512, 512, (err, image) => {
+            resolve(image);
+        });
+    });
+}
+function jimRead(url, count) {
     let date = new Date();
-    const ddate = date.getTime() + date.getTimezoneOffset() * 60 * 1000;
     const offset = count * 10 * 60 * 1000;
+    const ddate = date.getTime() + date.getTimezoneOffset() * 60 * 1000 - offset;
     const day = dateFormat(ddate, 'yyyy-mm-dd').concat('T').concat(dateFormat(ddate, 'HH')).concat(':', getMin(ddate, offset));
     const query = `appid=${appid}&day=${day}`;
-    return Promise.all([
-        Jimp.read(`https://c.sat.owm.io/maps/2.0/radar/7/74/42?${query}`),
-        Jimp.read(`https://c.sat.owm.io/maps/2.0/radar/7/75/42?${query}`),
-        Jimp.read(`https://c.sat.owm.io/maps/2.0/radar/7/74/43?${query}`),
-        Jimp.read(`https://c.sat.owm.io/maps/2.0/radar/7/75/43?${query}`),
-    ])
+    return Jimp.read(`${url}?${query}`)
         .catch(err => {
         if (count < 5) {
-            return loadRadar(count + 1);
+            return jimRead(url, count + 1);
         }
+        else {
+            return jimpCreate256();
+        }
+    });
+}
+const loadRadar = () => {
+    return Promise.all([
+        jimRead(`https://c.sat.owm.io/maps/2.0/radar/7/74/42`, 0),
+        jimRead(`https://c.sat.owm.io/maps/2.0/radar/7/75/42`, 0),
+        jimRead(`https://c.sat.owm.io/maps/2.0/radar/7/74/43`, 0),
+        jimRead(`https://c.sat.owm.io/maps/2.0/radar/7/75/43`, 0),
+    ])
+        .catch(err => {
+        console.error(err);
         return Promise.reject(err);
     });
 };
 exports.rain = (req, res, next) => {
-    loadRadar(0).then(([image1, image2, image3, image4]) => {
+    loadRadar().then(([image1, image2, image3, image4]) => {
         new Jimp(512, 512, (err, image) => {
             // this image is 256 x 256, every pixel is set to 0x00000000
             image.composite(image1, 0, 0)
--- server/openweathermap/openweatherrain.ts
+++ server/openweathermap/openweatherrain.ts
@@ -13,32 +13,51 @@ const getMin = (date: number, offset): string => {
    return `${m}`;
 };
 //https://b.sat.owm.io/maps/2.0/radar/7/75/43?appid=9de243494c0b295cca9337e1e96b00e2&day=2021-05-02T22:00
-const loadRadar = (count: number) => {
-    console.log(count)
+
+function jimpCreate256() {
+    return new Promise((resolve) => {
+        new Jimp(512, 512, (err, image) => {
+            resolve(image)
+        })
+    })
+}
+
+function jimRead(url: string, count: number) {
     let date = new Date();
-    const ddate = date.getTime() + date.getTimezoneOffset()*60*1000;
     const offset = count * 10 * 60 *1000;
+    const ddate = date.getTime() + date.getTimezoneOffset()*60*1000 - offset;
+
     const day = dateFormat(ddate, 'yyyy-mm-dd').concat('T').concat(dateFormat(ddate, 'HH')).concat(':',getMin(ddate, offset));
     const query = `appid=${appid}&day=${day}`;
+    return Jimp.read(`${url}?${query}`)
+        .catch(err => {
+            if(count<5){
+                return jimRead(url, count+1)
+            }
+            else {
+                return  jimpCreate256()
+            }
+        })
+}
+const loadRadar = () => {
+
     return Promise.all(
         [
-            Jimp.read(`https://c.sat.owm.io/maps/2.0/radar/7/74/42?${query}`),
-            Jimp.read(`https://c.sat.owm.io/maps/2.0/radar/7/75/42?${query}`),
-            Jimp.read(`https://c.sat.owm.io/maps/2.0/radar/7/74/43?${query}`),
-            Jimp.read(`https://c.sat.owm.io/maps/2.0/radar/7/75/43?${query}`),
+            jimRead(`https://c.sat.owm.io/maps/2.0/radar/7/74/42`, 0),
+            jimRead(`https://c.sat.owm.io/maps/2.0/radar/7/75/42`, 0),
+            jimRead(`https://c.sat.owm.io/maps/2.0/radar/7/74/43`, 0),
+            jimRead(`https://c.sat.owm.io/maps/2.0/radar/7/75/43`, 0),
         ]
     )
         .catch(err => {
-            if(count<5){
-                return loadRadar(count+1)
-            }
+            console.error(err)
             return Promise.reject(err)
         })
 
 };
 
 export const rain = (req: any, res: any, next: any) => {
-    loadRadar(0).then(([image1, image2, image3, image4]) => {
+    loadRadar().then(([image1, image2, image3, image4]) => {
         new Jimp(512, 512, (err, image) => {
             // this image is 256 x 256, every pixel is set to 0x00000000
             image.composite(image1, 0, 0)
