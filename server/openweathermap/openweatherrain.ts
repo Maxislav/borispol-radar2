a2c89888bd12d3239ce3be2a39a0884ed358368b
Maxislav <Maksym.Lipatov@McKesson.com>
2021-05-12 19:23:26 +0300

    replace color

--- server/openweathermap/openweatherrain.ts
+++ server/openweathermap/openweatherrain.ts
@@ -51,7 +51,7 @@ function jimpCreate256() {
 
 function jimRead(url: string, count: number) {
     let d = new Date();
-    const offset = (count-1) * 10 * 60 * 1000 + 10 * 60 * 1000;
+    const offset = (count - 1) * 10 * 60 * 1000 + 10 * 60 * 1000;
     const date = d.getTime() + d.getTimezoneOffset() * 60 * 1000 - offset;
 
     const day = dateFormat(date, 'yyyy-mm-dd').concat('T').concat(dateFormat(date, 'HH')).concat(':', getMin(date));
@@ -103,25 +103,51 @@ export const rain = (req: any, res: any, next: any) => {
                               ]) => {
         new Jimp(768, 512, (err, image) => {
             // this image is 256 x 256, every pixel is set to 0x00000000
-            image
+            const srcImage = image
                 .composite(image1, 0, 0)
                 .composite(image2, 256, 0)
                 .composite(image3, 512, 0)
                 .composite(image4, 0, 256)
                 .composite(image5, 256, 256)
                 .composite(image6, 512, 256)
-                .crop(128, 0, 640, 512)
-                .getBufferAsync(Jimp.MIME_PNG)
-                .then(buffer => {
-                    res.header("Access-Control-Allow-Origin", "*");
-                    res.header("Content-Type", "image/png");
-                    res.send(buffer);
-                })
-                .catch(err => {
-                    console.error('err composite');
-                    res.status(500);
-                    res.send('error', {error: err});
-                });
+                .crop(128, 0, 640, 512);
+
+            const srcColor1 = Jimp.intToRGBA(0xFA64ff);
+            const srcColor2 = Jimp.intToRGBA(0xE600ff);
+            const srcColor3 = Jimp.intToRGBA(0xBA00ff);
+            srcImage.scan(0, 0, srcImage.bitmap.width, srcImage.bitmap.height, function (x, y, idx) {
+                // do your stuff..
+                const r = this.bitmap.data[idx + 0];
+                const g = this.bitmap.data[idx + 1];
+                const b = this.bitmap.data[idx + 2];
+                const a = this.bitmap.data[idx + 3];
+                if (match(srcColor1, r, g, b)) {
+                    //   const c = Jimp.rgbaToInt(r, g, b, a)
+
+                    this.bitmap.data[idx + 3] = 180;
+                }
+                if(match(srcColor3, r,g,b)){
+                 /*   this.bitmap.data[idx + 0] = 66;
+                    this.bitmap.data[idx + 1] = 136;
+                    this.bitmap.data[idx + 2] = 229;
+                    this.bitmap.data[idx + 3] = 255;
+*/                }
+
+            }, () => {
+                srcImage.getBufferAsync(Jimp.MIME_PNG)
+                    .then(buffer => {
+                        res.header("Access-Control-Allow-Origin", "*");
+                        res.header("Content-Type", "image/png");
+                        res.send(buffer);
+                    })
+                    .catch(err => {
+                        console.error('err composite');
+                        res.status(500);
+                        res.send('error', {error: err});
+                    });
+            });
+
+
         });
 
     })
@@ -131,4 +157,9 @@ export const rain = (req: any, res: any, next: any) => {
             res.send('error', {error: err});
         })
 
+}
+
+
+function match(srcColor, r: number, g: number, b: number) {
+ return r < srcColor.r + 10 && srcColor.r - 10 < r && g < srcColor.g + 10 && srcColor.g - 10 < g && b < srcColor.b + 10 && srcColor.b - 10 < b
 }
