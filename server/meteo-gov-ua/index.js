0fbe2c8560b1a472d717fb7722207579b35684c6
Maxislav <Maksym.Lipatov@McKesson.com>
2021-09-15 15:52:56 +0300

    debug

--- server/meteo-gov-ua/index.js
+++ server/meteo-gov-ua/index.js
@@ -41,8 +41,7 @@ const httpGet = () => {
     });
 };
 const meteoGovUa = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log('req.hostname ->>', req.hostname);
-    if (req.hostname.match(/(178\.62\.44\.54)(meteo-info\.kiev\.ua)|(localhost)/g)) {
+    if (req.hostname.match(/(178\.62\.44\.54)|(meteo-info\.kiev\.ua)|(localhost)/g)) {
         res.header('Access-Control-Allow-Origin', '*');
         const d = (yield httpGet()).toString();
         const matchers = d.match(/time_radar\s?=\s?"(\d|\s|-)+/g);
