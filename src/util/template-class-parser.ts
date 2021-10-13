f68e2f103d9f6571c2ac7597458ca94f499d5082
Maxislav <Maksym.Lipatov@McKesson.com>
2021-10-13 16:36:18 +0300

    add template parser

--- src/util/template-class-parser.ts
+++ src/util/template-class-parser.ts
@@ -1,13 +1,6 @@
-import { parseStringTemplateGenerator, evaluateParsedString } from 'string-template-parser';
-
-
-const parseAngularStringTemplate = parseStringTemplateGenerator({
-    VARIABLE_START: /^\$\{\s*/,
-    VARIABLE_END: /^\s*\}/,
-});
-
+import { evaluateStringTemplate } from 'string-template-parser';
 export const templateClassParse = (template: string, style: {[key: string]: any}) => {
-    return evaluateParsedString(parseAngularStringTemplate(template), {
+    return evaluateStringTemplate(template, {
         ...style,
     } ,null)
 }
