7bed6d0f866270dedecea4e03aeb5d3b12819d04
Maxislav <Maksym.Lipatov@McKesson.com>
2021-10-15 15:56:18 +0300

    add gismeteo info

--- src/component/informer-component/informer-component.ts
+++ src/component/informer-component/informer-component.ts
@@ -1,30 +1,34 @@
 import {borispolComponent} from '../../util/borispol-component';
 import template from './informer-component.html'
 import style from './informer-component.less'
-
+let loaded = false;
 export const informerComponent = borispolComponent('informer-component' ,{
     template,
     style,
     mounted(): void {
-        (function() {
-            const d = this.document,
-                o = this.navigator.userAgent.match(/MSIE (6|7|8)/) ? true : false,
-                s = d.createElement('script');
-            s.src  = 'https://www.gismeteo.ua/informers/simple/install/';
-            s.type = 'text/javascript';
-            s[(o ? 'defer': 'async')] = true;
-            s[(o ? 'onreadystatechange' : 'onload')] = function() {
-                const GmI: any = window['GmI'];
+            (function() {
+                const d = this.document,
+                    o = this.navigator.userAgent.match(/MSIE (6|7|8)/) ? true : false,
+                    s = d.createElement('script');
 
-                try {new GmI({
-                    slug : 'f60fc683b425215be19ccbc52785a242',
-                    type : '240x90-2',
-                    city : '4944',
-                    lang : 'ru',
-                })} catch(e) {}
-            };
+                s.src  = 'https://www.gismeteo.ua/api/informer/getinformer/?hash=Qmw1fy8cwa3o0A';
+                s.type = 'text/javascript';
+                s[(o ? 'defer' : 'async')] = true;
+                s[(o ? 'onreadystatechange' : 'onload')] = function() {
 
-            d.body.appendChild(s);
-        }).call(window);
+                };
+
+                d.body.appendChild(s)
+                if(!loaded){
+                    const link = d.createElement('link');
+                    link.setAttribute('rel', 'stylesheet');
+                    link.setAttribute('type', 'text/css');
+                    link.setAttribute('href', 'https://www.gismeteo.ua/assets/flat-ui/legacy/css/informer.min.css');
+                    d.head.appendChild(link);
+                    loaded = true
+                }
+
+            }).call(window);
+
     },
 });
