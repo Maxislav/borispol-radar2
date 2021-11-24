f41bf32e166759cc7cca6d27571ed16f8fd8a428
Maxim Lipatov <maximmalyshyn@gmail.com>
2021-11-24 16:51:03 +0200

    change config

--- server/phplike/settingborispolradar.js
+++ server/phplike/settingborispolradar.js
@@ -3,12 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.defaultSetting = void 0;
 function defaultSetting(req, res) {
     res.send({
-        'title1': "Borispol",
-        'title2': "Infrared",
-        'title3': "Visible",
-        'url1': "https://meteoinfo.by/radar/UKBB/UKBB_latest.png",
-        'url2': "http://www.sat24.com/image2.ashx?region=eu&ir=true",
-        'url3': "http://www.sat24.com/image2.ashx?region=eu"
+        'title1': 'Borispol',
+        'title2': 'Infrared',
+        'title3': 'Visible',
+        'url1': 'http://178.62.44.54:8084/meteo-gov-ua/1',
+        'url2': 'http://www.sat24.com/image2.ashx?region=eu&ir=true',
+        'url3': 'http://www.sat24.com/image2.ashx?region=eu',
     });
 }
 exports.defaultSetting = defaultSetting;
--- server/phplike/settingborispolradar.ts
+++ server/phplike/settingborispolradar.ts
@@ -1,13 +1,12 @@
-
-export function defaultSetting(req, res) {
-
+import {Request, Response} from 'express-serve-static-core';
+export function defaultSetting(req: Request, res: Response) {
     res.send({
-        'title1' : "Borispol",
-        'title2' : "Infrared",
-        'title3' : "Visible",
-        'url1' : "https://meteoinfo.by/radar/UKBB/UKBB_latest.png",
-        'url2' : "http://www.sat24.com/image2.ashx?region=eu&ir=true",
-        'url3' : "http://www.sat24.com/image2.ashx?region=eu"
+        'title1': 'Borispol',
+        'title2': 'Infrared',
+        'title3': 'Visible',
+        'url1': 'http://178.62.44.54:8084/meteo-gov-ua/1',
+        'url2': 'http://www.sat24.com/image2.ashx?region=eu&ir=true',
+        'url3': 'http://www.sat24.com/image2.ashx?region=eu',
     })
 }
 
