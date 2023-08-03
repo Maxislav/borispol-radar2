0e384f4fca78f5c32a4a8f0673ac1dce145e603a
Maksym_Lipatov <maximmalyshyn@gmail.com>
2023-08-03 09:16:50 +0300

    pm2 update

--- server/pm2.utility.config.js
+++ server/pm2.utility.config.js
@@ -10,6 +10,13 @@ const pm2Config = {
                 NODE_ENV: "dev"
             }
         },
+        {
+            name: 'openrain',
+            script: './server/openweathermap/index.js',
+            exec_mode: 'cluster_mode',
+            max_memory_restart: '200M',
+            instances: 1,
+        },
     ],
 };
 
